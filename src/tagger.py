379d87f5b6aa52902cd8e25b4da22e4a58ea062b
Toshaan Bhale <toshaanbhale@gmail.com>
2026-03-31 19:54:10 +0530

    Implement Tagger class for text prediction
    Added Tagger class to load model and vectorizer for predictions.

--- src/tagger.py
+++ src/tagger.py
@@ -1,1 +1,15 @@
+import pickle
+from src.preprocess import clean_text
 
+class Tagger:
+    def __init__(self):
+        with open("models/model.pkl", "rb") as f:
+            self.model = pickle.load(f)
+
+        with open("models/vectorizer.pkl", "rb") as f:
+            self.vectorizer = pickle.load(f)
+
+    def predict(self, text):
+        text = clean_text(text)
+        vec = self.vectorizer.transform([text])
+        return self.model.predict(vec)[0]
