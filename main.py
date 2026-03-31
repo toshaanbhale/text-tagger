09f479f8b491e1e53ca740aa0ad2355c3510f72f
Toshaan Bhale <toshaanbhale@gmail.com>
2026-03-31 19:56:52 +0530

    Add command-line interface for tagging functionality

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+import argparse
+from src.tagger import Tagger
+from src.utils import read_file
 
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file", help="Path to text file")
+    args = parser.parse_args()
+
+    text = read_file(args.file)
+
+    tagger = Tagger()
+    tag = tagger.predict(text)
+
+    print("\n📄 File:", args.file)
+    print("🏷️ Predicted Tag:", tag)
+
+if __name__ == "__main__":
+    main()
